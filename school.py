74b79929881bcb992b5ef9c1469a49ef1b9df5a0
linhuangwei <42707526+linhuangwei@users.noreply.github.com>
2019-02-20 15:26:14 +0800

    first

---
+++ school.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import bs4
+
+def getHTMLText(url):
+    try:
+        r = requests.get(url,timeout = 30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
+
+def fillUnivList(ulist,html):
+    soup = BeautifulSoup(html,"html.parser")
+    for tr in soup.find('tbody').children:
+        if isinstance(tr,bs4.element.Tag):
+            tds = tr('td')
+            ulist.append([tds[0].string, tds[1].string,tds[2].string])
+
+def printUnivList(ulist,num):
+    print("{0:^8}\t{1:{3}^10}\t{2:^10}".format("排名","学校","省市",chr(12288)))
+    for i in range(num):
+        u=ulist[i]
+        print("{0:^10}\t{1:{3}^10}\t{2:^10}".format(u[0],u[1],u[2],chr(12288)))
+
+if __name__ == '__main__':
+    uinfo = []
+    url = 'http://www.zuihaodaxue.com/zuihaodaxuepaiming2018.html'
+    html = getHTMLText(url)
+    fillUnivList(uinfo,html)
+    printUnivList(uinfo,100)
